634df20b80c2aef7048138024bc53f55d9d376a5
shirshak55 <shirshak55@gmail.com>
2022-03-28 13:07:45 +0545

    Add support for more books

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,38 @@
 import delay from "delay"
 import { chromium } from "playwright"
 import path from "path"
+import process from "process"
+import toml from "toml"
+import fs from "fs"
 
 declare const document: any
 
 async function main() {
+    let config = toml.parse(fs.readFileSync("./config.toml").toString())
+
     let browser = await chromium.launch({ headless: true })
     let context = await browser.newContext()
     let page = await context.newPage()
 
-    await page.goto("https://doc.rust-lang.org/book/print.html")
-    await page.evaluate(() => {
-        let head = document.querySelector("head")
-        let link = document.createElement("link")
-        link.href = "https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;900&display=swap"
-        link.rel = "stylesheet"
-        head.appendChild(link)
+    for (let book_key in config.Books) {
+        let book = config.Books[book_key]
 
-        let link2 = document.createElement("link2")
-        link2.href = "   https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
-        link2.rel = "stylesheet"
-        head.appendChild(link2)
+        console.log("Download from", book_key)
 
-        var styles = `
+        await page.goto(book.print_url)
+        await page.evaluate(() => {
+            let head = document.querySelector("head")
+            let link = document.createElement("link")
+            link.href = "https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;900&display=swap"
+            link.rel = "stylesheet"
+            head.appendChild(link)
+
+            let link2 = document.createElement("link2")
+            link2.href = "   https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
+            link2.rel = "stylesheet"
+            head.appendChild(link2)
+
+            var styles = `
             html {
                 font-family: Nunito
             }
@@ -40,29 +50,30 @@ async function main() {
             }
         `
 
-        var styleSheet = document.createElement("style")
-        styleSheet.type = "text/css"
-        styleSheet.innerText = styles
-        document.head.appendChild(styleSheet)
-    })
+            var styleSheet = document.createElement("style")
+            styleSheet.type = "text/css"
+            styleSheet.innerText = styles
+            document.head.appendChild(styleSheet)
+        })
 
-    console.log("Delay for 30 seconds")
-    await delay(30 * 1000)
+        console.log("Delay for 30 seconds")
+        await delay(30 * 1000)
 
-    let dest = path.resolve(`./rust_book_${new Date().toDateString().split(" ").join("_").toLowerCase()}.pdf`)
-    await page.pdf({
-        path: dest,
-        format: "a4",
-        printBackground: true,
-        margin: {
-            top: 40,
-            bottom: 40,
-            left: 25,
-            right: 25,
-        },
-    })
+        let dest = path.resolve(book.file_name)
+        await page.pdf({
+            path: dest,
+            format: "a4",
+            printBackground: true,
+            margin: {
+                top: 40,
+                bottom: 40,
+                left: 25,
+                right: 25,
+            },
+        })
 
-    console.log(`Succesfully printed? ${dest}`)
+        console.log(`Succesfully printed? ${dest}`)
+    }
 
     console.log("Completed")
     await browser.close()
